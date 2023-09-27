0f1457a87ec05723af3addb96ccc1bb6fe41af7e
Smagul.Alkey <roket.flesh@gmail.com>
2023-09-27 15:22:14 +0600

    bas

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,17 @@
 package service
+
+import "TestProjectEthereum/internal/repository"
+
+type Service struct {
+	OperationServiceIn
+}
+
+func NewService(repository *repository.Repository) *Service {
+	return &Service{
+		OperationServiceIn: NewOperationService(repository),
+	}
+}
+
+type OperationServiceIn interface {
+	UpdateBalance(balance int32) error
+}
