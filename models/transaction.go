6d5fbade39f3d3461efcec090b3daeffd7b9e0c1
Smagul.Alkey <roket.flesh@gmail.com>
2023-10-05 12:05:42 +0600

    fixed some bugs

--- models/transaction.go
+++ models/transaction.go
@@ -1,10 +1,15 @@
 package models
 
+import (
+	"math/big"
+)
+
 type Transaction struct {
-	Value      string `json:"value"`
-	PrivateKey string `json:"private_key"`
-	AddressTo  string `json:"address"`
-	Hex        string `json:"hex,omitempty"` // this is no need on frontend
+	Value      big.Int `json:"value"`
+	PrivateKey string  `json:"private_key"`
+	AddressTo  string  `json:"address"`
+	Hex        string  `json:"hex,omitempty"`
+	// this is no need on frontend
 }
 
 const (
